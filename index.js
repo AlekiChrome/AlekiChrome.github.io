cbc2e597d5013850934f6fbad200daad217ffe3f
Candice Agard <candiceagard@candices-mbp.myfiosgateway.com>
2021-03-22 09:29:04 -0400

    footer section added for mobile and desktop view, response is delayed

--- index.js
+++ index.js
@@ -35,4 +35,25 @@ VanillaTilt.init(document.querySelectorAll(".card"), {
     glare: true,
     "max-glare": 1
 })
+
+$('nav a[href*="#"]').on('click', function () {
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top - 100
+    }, 2000);
+ });
+
+
+ $('#up').on('click', function () {
+     $('html, body').animate({
+         scrollTop: 0
+     }, 2000);
+ });
+
+
+ AOS.init({
+     easing: 'ease',
+     duration: 1800,
+     // once: true
+ });
+
 })
